0975ba37a18025ab498bea0d8c446970f521a929
Dmitry Shevchenko <dmishe@gmail.com>
2010-03-05 12:06:53 +0200

    code cleanup, pep8ify

--- debug_toolbar/panels/profiling.py
+++ debug_toolbar/panels/profiling.py
@@ -1,33 +1,31 @@
-import sys
 import tempfile
 import hotshot
 import hotshot.stats
-from cStringIO import StringIO
 
 
-from django.conf import settings
 from django.template.loader import render_to_string
 from django.utils.translation import ugettext_lazy as _
 from debug_toolbar.panels import DebugPanel
-from debug_toolbar.debug.headers import DebugHeaders
+
 
 def f8(x):
+    """Helper function for time formatting"""
     return "%8.3f" % x
+
 
 class ProfilingPanel(DebugPanel):
     """
-    A panel to display HTTP headers.
+    A panel to display Profiling info.
     """
     name = 'Profiling'
     has_content = True
 
-    def __init__(self, context={}):
+    def __init__(self, context):
         super(ProfilingPanel, self).__init__(context)
-
 
     def nav_title(self):
         return _('Profiling')
-        
+
     def nav_subtitle(self):
         return '%s function calls' % self.stats.total_calls
 
@@ -43,36 +41,28 @@ class ProfilingPanel(DebugPanel):
             'stats': self.stats,
             'list': self.list,
             })
-        
+
         return render_to_string('debug_toolbar/panels/profiling.html', context)
 
     def process_request(self, request):
         self.tmpfile = tempfile.NamedTemporaryFile()
         self.prof = hotshot.Profile(self.tmpfile.name)
 
-    
     def process_view(self, request, callback, callback_args, callback_kwargs):
         return self.prof.runcall(callback, request, *callback_args, **callback_kwargs)
 
-   
     def process_response(self, request, response):
         self.prof.close()
 
         stats = hotshot.stats.load(self.tmpfile.name)
-        #stats.strip_dirs()
         stats.sort_stats('cumulative')
-        
-        
 
-        self.stats = stats        
+        self.stats = stats
+
         self.list = []
-        
         for func in self.stats.get_print_list({})[1]:
-            cc, nc, tt, ct, callers = self.stats.stats[func]
-            self.list.append((cc, nc, f8(tt), f8(ct), func[2], func[0], func[1]))
-        
+            cc, nc, tt, ct, _ = self.stats.stats[func]
+            self.list.append(
+                (cc, nc, f8(tt), f8(ct), func[2], func[0], func[1]))
+
         return response
-
-
-    
-
